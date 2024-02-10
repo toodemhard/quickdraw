e0ebaa2daa238ae2f7602a67f901bc7c979d0e4a
toodemhard <lailokkay@gmail.com>
2024-02-10 18:52:12 +0000

    idk what this was

---
+++ src/color.ts
@@ -0,0 +1,13 @@
+export function color_picker_setup() {
+    const colorPicker = document.getElementById("color-picker");
+}
+
+export function wtf() {
+    console.log("fsdf");
+}
+
+type color = {
+    r: number;
+    g: number;
+    b: number;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,46 @@
-import './style.css'
+import { wtf } from "./color";
 
-function drawEvent(e: MouseEvent) {
-  if (e.buttons !== 1) {
-    return;
-  }
-  interp({ x: e.offsetX, y: e.offsetY });
-  // ctx?.beginPath();
-  // ctx?.arc(e.offsetX, e.offsetY, 2, 0, Math.PI * 2);
-  // ctx?.fill();
+function drawEvent(e: PointerEvent) {
+    // console.log(`${e.offsetX}, ${e.offsetY}`);
+    // interp({ x: e.x, y: e.y });
+    circleBrush({ x: e.offsetX, y: e.offsetY }, e.pressure);
+    // if (ctx) {
+    //   console.log("jf");
+    // }
+}
+
+let setint: number;
+
+function onMouseDown(e: PointerEvent) {
+    lastPoint = { x: e.offsetX, y: e.offsetY };
+    setint = setInterval(function () {
+        drawEvent(idk);
+    }, 1);
+}
+
+function onMouseUp() {
+    console.log("up");
+    clearInterval(setint);
+}
+
+function circleBrush(pos: Vector, pressure: number) {
+    console.log(pressure);
+    // if (ctx) {
+    //   ctx.fillStyle = "rgb(0 255 255)"
+    // }
+
+    ctx?.beginPath();
+    ctx?.arc(pos.x, pos.y, scale * pressure, 0, Math.PI * 2);
+    ctx?.fill();
 }
 
 function interp(pos: Vector) {
-  ctx?.beginPath();
-  ctx?.moveTo(lastPoint.x, lastPoint.y);
-  ctx?.lineTo(pos.x, pos.y);
-  ctx?.stroke();
+    ctx?.beginPath();
+    ctx?.moveTo(lastPoint.x, lastPoint.y);
+    ctx?.lineTo(pos.x, pos.y);
+    ctx?.stroke();
 
-  lastPoint = { x: pos.x, y: pos.y };
-
+    lastPoint = { x: pos.x, y: pos.y };
 }
 
 // function squareBrush() {
@@ -25,16 +48,67 @@ function interp(pos: Vector) {
 // }
 
 type Vector = {
-  x: number;
-  y: number;
-}
+    x: number;
+    y: number;
+};
 
 let lastPoint: Vector = { x: 0, y: 0 };
+
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 
-canvas.addEventListener("mousemove", drawEvent);
+const scaleSlider = document.getElementById("scale-slider") as HTMLInputElement;
+const output = document.getElementById("scale-output");
 
+let scale = 0;
+
+document.addEventListener("keydown", function (e) {
+    if (e.key == "[") {
+        changeScale(scale - 5);
+    }
+    if (e.key == "]") {
+        changeScale(scale + 5);
+    }
+});
+
+function clamp(number: number, min: number, max: number) {
+    return Math.max(min, Math.min(number, max));
+}
+
+function changeScale(value: number) {
+    value = clamp(value, 0, 500);
+    scale = value;
+    if (output) {
+        output.innerText = String(value);
+    }
+    if (scaleSlider) {
+        scaleSlider.value = String(value);
+    }
+}
+
+scaleSlider.oninput = function () {
+    console.log(scaleSlider.value);
+    changeScale(Number(scaleSlider.value));
+};
+
+if (ctx) {
+    ctx.lineWidth = 2;
+}
+
+// canvas.addEventListener("mousemove", drawEvent);
+// canvas.addEventListener("pointerdown", onMouseDown);
+// canvas.addEventListener("pointerup", onMouseUp);
+// canvas.addEventListener("pointerleave", onMouseUp);
 // ctx?.arc(50, 50, 20, 0, Math.PI * 2);
 // ctx?.fill();
 // console.log("jkadsjhf");
+
+let idk: PointerEvent;
+
+canvas.onpointermove = function (e: PointerEvent) {
+    if (e.pressure > 0) {
+        drawEvent(e);
+    }
+};
+
+wtf();
