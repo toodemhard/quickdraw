1b2db35f58c3d893adf7feb99b163acc7934ebda
lailokkei <lailokkay@gmail.com>
2024-04-09 17:39:38 +0100

    proxy api to go server

--- main.go
+++ main.go
@@ -3,10 +3,32 @@ package main
 import (
 	"net/http"
 	"os"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	port := os.Getenv("PORT")
-	http.Handle("/", http.FileServer(http.Dir("./dist")))
-	http.ListenAndServe(":"+port, nil)
+
+	isDev := false
+	if len(os.Args) == 2 && os.Args[1] == "--dev" {
+		port = "8080"
+		isDev = true
+	}
+
+	e := echo.New()
+
+	e.GET("/api", func(c echo.Context) error {
+		return c.String(http.StatusOK, "bnutinb")
+	})
+
+	// e.POST("/api/drawing", func(c echo.Context) error {
+	// })
+
+	if !isDev {
+		e.Static("/", "dist")
+	}
+
+	e.Logger.Fatal(e.Start(":" + port))
+
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solid()],
-})
+    plugins: [solid()],
+    server: {
+        proxy: {
+            "/api" : {
+                target: "http://localhost:8080"
+            }
+        }
+    }
+});
