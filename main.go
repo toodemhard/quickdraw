15c83a1c5b81398e4bd0129fd07e01ee5e858440
lailokkei <lailokkay@gmail.com>
2024-04-14 19:11:18 +0100

    Handle "PORT" env not existing

--- main.go
+++ main.go
@@ -1,33 +1,28 @@
 package main
 
 import (
-	"net/http"
 	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+	port, ok := os.LookupEnv("PORT")
 
-	isDev := false
-	if len(os.Args) == 2 && os.Args[1] == "--dev" {
+	if !ok {
 		port = "8080"
-		isDev = true
 	}
 
 	e := echo.New()
 
-	e.GET("/api", func(c echo.Context) error {
-		return c.String(http.StatusOK, "bnutinb")
-	})
-
+	// e.GET("/api", func(c echo.Context) error {
+	// 	return c.String(http.StatusOK, "bnutinb")
+	// })
+	//
 	// e.POST("/api/drawing", func(c echo.Context) error {
 	// })
 
-	if !isDev {
-		e.Static("/", "dist")
-	}
+	e.Static("/", "dist")
 
 	e.Logger.Fatal(e.Start(":" + port))
 
