4e1bca974938d9a7223085181430cd9f88ceadda
toodemhard <lailokkay@gmail.com>
2024-11-01 19:28:18 +0000

    zzz

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import solid from "vite-plugin-solid";
 
 export default defineConfig({
     plugins: [solid()],
+    build: {
+        outDir: "docs/"
+    },
     server: {
         proxy: {
             "/api" : {
                 target: "http://localhost:8080"
             }
         }
-    }
+    },
+    base: "/quickdraw/"
 });
